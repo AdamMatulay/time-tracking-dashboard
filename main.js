6cb09e82f8fac7be681391624722e95fa3d8ab78
AdamMatulay <adammatulay@proton.me>
2025-10-31 13:41:27 +0100

    data.json path fix

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const monthlyButton = document.querySelector(".toggle-button.monthly");
 
 let data = [];
 
-fetch("/data.json")
+fetch("./data.json")
   .then((response) => {
     if (!response.ok) throw new Error("DATA.JSON FAILED!");
 
